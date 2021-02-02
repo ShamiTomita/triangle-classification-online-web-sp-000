b6d58e85c1dcd4072843296c5739c4a3f0718a31
Shami Tomita <shamitomita@gmail.com>
2021-02-02 22:48:05 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,6 +8,17 @@ class Triangle
   end 
   
   def kind(side1, side2, side3)
+    #given 3 sides, this method should return what type of triangle is being inputted
+    #the boundaries of this should be that all sides must be larger than 0 and that two sides of a triangle must exceed the length of the third 
+    if side1 <= 0 || side2 <= 0 || side3 <=0 #if any side is less than or equal to 0, raise an error 
+    elsif side1 + side2 < side3 || side3 + side1 < side2 || side2 + side3 < side1
+  end 
+  
+  
+  
+  
+  
+  def kind(side1, side2, side3)
     if side1 <= 0 || side2 <= 0 || side3 <=0
       begin 
         raise TriangleError
